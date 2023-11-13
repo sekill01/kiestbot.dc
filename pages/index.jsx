d8640cd7053166441be23f8e100503235d3482cc
sekill01 <80636880+sekill01@users.noreply.github.com>
2023-11-13 18:17:43 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,13 +17,13 @@ export default function Index() {
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-            <Link href={"https://bit.ly/3PHDjyC"}>
+            <Link href={"https://bit.ly/47rEPwY"}>
               <a
                 className={
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
-          Invite Astra
+          Kiest Botu Ekle
               </a>
             </Link>
   <div className="py-10"></div>
