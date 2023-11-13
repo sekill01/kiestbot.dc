f930181cf1606ed817c9f57a0299d65ce383584b
sekill01 <80636880+sekill01@users.noreply.github.com>
2023-11-13 18:02:35 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,27 +8,28 @@ export default function Index() {
       <div className="py-20 mb-30 max-w-3xl mx-auto">
         <div className="flex w-full justify-center">
       <img width="500" src="/img/bck.png" />
+      </div>
         <p className="animateHeader text-4xl font-extrabold text-center text-white">
-          Kiest Bot
+          Astra Bot
         </p>
         <p className="animateHeader text-white text-opacity-50 text-center mt-5">
-      Kiest bot ile Eğlenceye ne dersin? Kendi özgür dünyanızı yaratın ve kendi Discord içi oyun parasını oluşturun, bu keyfi en iyi siz yaşayın. 
+       How about moderating your own server with Astra bot? Create your own free world and create your own playlist, experience this pleasure with the best sounds. 
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-            <Link href={"https://discord.com/api/oauth2/authorize?client_id=1159522977830944788&permissions=8&scope=bot"}>
+            <Link href={"https://bit.ly/3PHDjyC"}>
               <a
                 className={
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
                 }
               >
-          Invite Kiest
+          Invite Astra
               </a>
             </Link>
   <div className="py-10"></div>
-  <Link href={"https://discord.com/api/oauth2/authorize?client_id=1159522977830944788&permissions=8&scope=bot"}>
+  <Link href={"https://bit.ly/3LYzaDe"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Oy ver</a>
+Vote Astra</a>
   </Link>
         </div>
 
@@ -46,8 +47,8 @@ Oy ver</a>
           <div className="col-span-4">
             <p className="text-white text-4xl font-bold" dangerouslySetInnerHTML={{ __html:"Astra Bot" }} />
             <p className="text-white text-md font-medium text-gray-500/75" dangerouslySetInnerHTML={{ __html: "How about creating the best environment to relax yourself with your own emotions?" }}/>
-            <a href="https://discord.com/api/oauth2/authorize?client_id=1159522977830944788&permissions=8&scope=bot" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
-              <i className="fab fa-discord mr-2" />Invite Kiest
+            <a href="https://bit.ly/3PHDjyC" className={"mt-10 flex items-center px-4 justify-center gap-x-2 shadow-lg shadow-amber-600/20 cursor-pointer rounded-xl py-4 font-medium bg-gradient-to-r from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white"}>
+              <i className="fab fa-discord mr-2" />Invite Astra
             </a>
           </div>
 
@@ -55,26 +56,26 @@ Oy ver</a>
            
               <div>
                 <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimizasyon</p>
-                  <p className="text-gray-500 line-clamp-4">Kiest, sunucunuza özel hızlı ve eğlenceli . Resmen üst bir seviye.</p>
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-cogs text-2xl text-amber-500`} /> Optimization</p>
+                  <p className="text-gray-500 line-clamp-4">With fast and customizable settings specific to your server, Astra gives you a musical pleasure to the fullest.</p>
                 </div>
               </div>
               <div>
                 <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Dil</p>
-                  <p className="text-gray-500 line-clamp-4">Kiest Botumuz Tüm Dilleri Destekleyen Bir Bottur (Yakın Zamanda Diller Eklenecek).)</p>
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-universal-access text-2xl text-amber-500`} /> Universal</p>
+                  <p className="text-gray-500 line-clamp-4">A bot that supports multi-platform music playback. (Spotify, Souncloud, YouTube) | (It is a bot that complies with all copyrights and licenses.)</p>
                 </div>
               </div>
               <div>
                 <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Güvenlik</p>
-                  <p className="text-gray-500 line-clamp-4">Sunucunuzu Patlatmak için bir sebep duymuyoruz yani bizde bu işi eğlence amaçlı yapıyoruz.</p>
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-shield text-2xl text-amber-500`} /> Security</p>
+                  <p className="text-gray-500 line-clamp-4">An open source safe bot that you can browse and help us develop.</p>
                 </div>
               </div>
               <div>
                 <div className="text-white">
-                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-ticket text-2xl text-amber-500`} /> Destek</p>
-                  <p className="text-gray-500 line-clamp-4">Bot'u tamamen bilen ve bot üzerinde bilgili olan yetkili ekibimiz'le sizlere daha kaliteli Destek ekibi sunuyoruz.</p>
+                  <p className="text-xl font-semibold mt-5"><i className={`fa fa-ticket text-2xl text-amber-500`} /> Support</p>
+                  <p className="text-gray-500 line-clamp-4">With our best teammates, we provide the fastest response to anyone who needs help.</p>
                 </div>
               </div>
           </div>
@@ -85,11 +86,11 @@ Oy ver</a>
       <div className="flex flex-row w-full h-auto lg:h-56 my-14 lg:my-40 bg-gradient-to-br lg:px-30 from-neutral-900/90 flex items-center justify-start to-neutral-900/50 rounded-lg p-1 shadow-md">
               <>
                 <div className="lg:mx-4 lg:p-8 py-4 lg:py-16 flex items-center justify-center flex-col text-center w-full lg:w-auto lg:text-left lg:flex-none">
-                  <img src="https://cdn.discordapp.com/attachments/1164545587283238963/1173632467379441755/Screenshot_77.png?ex=6564a969&is=65523469&hm=f47ead70e79dac2f07377844b2a3c596f5025939740ba119e618ecd102d64da9&"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
-                  <p className="text-3xl text-white font-semibold">En İyi Tr Ekonomi Botu</p>
-                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Ticaret yapıp arkadaşların ile vakit geçirmeye hazırmısın ? o zaman botu kendi sunucuna davet et ve eğlence başlasın !</p>
+                  <img src="https://cdn.discordapp.com/attachments/737348411568685066/983094813115170866/unknown.png"  width="256" className="lg:hidden rounded-xl mb-5 shadow-xl shadow-black" />
+                  <p className="text-3xl text-white font-semibold">Best Your Bot</p>
+                  <p className="text-md text-white font-medium text-gray-500 line-clamp-5">Are you ready to throw the best parties with Astra, the best and most up-to-date bot?</p>
                 </div>
-                <img src="https://cdn.discordapp.com/attachments/1164545587283238963/1173632467379441755/Screenshot_77.png?ex=6564a969&is=65523469&hm=f47ead70e79dac2f07377844b2a3c596f5025939740ba119e618ecd102d64da9&" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
+                <img src="https://cdn.discordapp.com/attachments/737348411568685066/983094813115170866/unknown.png" className="hidden lg:block rounded-xl perspective-right shadow-xl shadow-black" />
               </>
       
         </div>
